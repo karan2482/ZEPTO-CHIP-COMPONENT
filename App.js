4602d83dbf1c0aaa90fd3bf62b8336ef74ab8dbe
KARAN KATHAYAT <115268833+karan2482@users.noreply.github.com>
2024-01-16 22:02:27 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import ChipComponent from './components/ChipComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Select UserForm</h1>
+   <ChipComponent></ChipComponent>
+    </div>
+  );
+}
+
+export default App;
